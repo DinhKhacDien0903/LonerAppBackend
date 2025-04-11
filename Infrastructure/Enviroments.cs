65e6b3e3b922d8320301ab7620c3dd6aa2ea7c4e
diendk <diendk@newwave.vn>
2025-04-12 01:25:00 +0700

    add gitIgnore

--- Infrastructure/Enviroments.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Infrastructure;
-
-public static class Enviroments
-{
-    public static string ConnectionString_Docker = "Server=localhost, 1433; Database=Loner;User Id=sa;Password=DinhKhacDien1009!@#;TrustServerCertificate=True;";
-    public static string ConnectionString_SSMS = "Server=localhost, 1433; Initial Catalog=Loner; Integrated Security=True; TrustServerCertificate=True;";
-}
