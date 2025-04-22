ee7ef29977cbcabdfb420b103a8a6fb3a8a001e8
diendk <diendk@newwave.vn>
2025-04-23 01:26:07 +0700

    clean

--- Loner.Application/DTOs/Auth.cs
+++ Loner.Application/DTOs/Auth.cs
@@ -8,7 +8,7 @@ public class Auth
     public record VerifyEmailRequest(string Email, string Otp, bool IsLoggingIn) : IRequest<Result<AuthResponse>>;
     public record LogoutRequest(string UserId, string RefreshToken) : IRequest<Result<LogoutResponse>>;
     public record DeleteAccountRequest(string UserId, string RefreshToken) : IRequest<Result<DeleteAccountResponse>>;
-    public record AuthResponse(string AccessToken, string RefreshToken, bool IsVerified, string UserId, bool IsSetupedAccount, bool IsAccountExisted);
+    public record AuthResponse(string AccessToken, string RefreshToken, bool IsVerified, string UserId, bool IsAccountSetup, bool IsAccountExisted);
     public record LogoutResponse(bool IsSuccess);
     public record DeleteAccountResponse(bool IsSuccess);
     public record SendOTPResponse(string Message, bool IsSuccess);
